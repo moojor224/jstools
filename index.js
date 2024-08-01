3e47cb235e54718527fa0dd7d1bcff90a5714888
moojor224 <moojor224@gmail.com>
2024-08-01 13:13:31 -0500

    wrap lines for better readability

--- index.js
+++ index.js
@@ -98,5 +98,8 @@ export { Overload } from "./src/overloaded_functions.js";
 export { Option, Section, Settings } from "./src/settings.js";
 export { advancedDynamicSort, dynamicSort } from "./src/sorting.js";
 export { tabColor } from "./src/tabColor.js";
-export { BULK_OPERATIONS, constant, copyObject, createEnum, extend, getBrowserType, getValueOrDefault, isAsync, lockValue, logAndReturn, logFormatted, makeTemplate, objectToJSONML, prismToJSONML, stringify, timeConversions, toHTMLEntities, wrapInQuotes } from "./src/utility.js";
+export { BULK_OPERATIONS, constant, copyObject, createEnum, extend,
+    getBrowserType, getValueOrDefault, isAsync, lockValue, logAndReturn,
+    logFormatted, makeTemplate, objectToJSONML, prismToJSONML, stringify,
+    timeConversions, toHTMLEntities, wrapInQuotes } from "./src/utility.js";
 export { waitForKeyElements } from "./src/waitForKeyElements.js";
