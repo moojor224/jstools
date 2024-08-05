7daa328706c9419c65c2e670fde0c6a324d7899c
moojor224 <moojor224@gmail.com>
2024-08-05 13:55:20 -0500

    rename workflow script file

---
+++ .github/scripts/update-index.mjs
@@ -0,0 +1,5 @@
+// console.log("\x1b[31mTHIS IS A TEST MESSAGE TO MAKE SURE THIS FILE RUNS");
+import * as fs from "fs";
+import * as path from "path";
+let indexPath = path.resolve("./index.html");
+console.log(indexPath);
