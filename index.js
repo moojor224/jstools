3b200034421ed3074e4d8a71bc926b93b2616935
moojor224 <moojor224@gmail.com>
2025-06-09 13:05:56 -0500

    Add customLogger

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ if (!Array.isArray(window.devtoolsFormatters)) {
 
 export { flattenChildNodes, flattenChildren, interleaveArrays, rectangle, reshape } from "./src/arrays.js";
 export { bulkElements } from "./src/bulkElements.js";
-export { captureConsole, consolePrefix } from "./src/console.js";
-export { CSSColors, Color, getColor, getContrastColor, gradient, listAllColorsOnPage, rgbMix } from "./src/colors.js";
+export { Color, CSSColors, getColor, getContrastColor, gradient, listAllColorsOnPage, rgbMix } from "./src/colors.js";
+export { captureConsole, customLogger } from "./src/console.js";
 export { createElement } from "./src/createElement.js";
 export { cssObjToString, jst_CSSRule, jst_CSSStyleSheet } from "./src/CSS.js";
 export { consoleButton } from "./src/devtoolsFormatters.js";
-export { CUSTOM_ELEMENTS, clear, clearError, clearWarn, disable, enable, error, hide, show, stringifyNodeTree, warn } from "./src/elements.js";
+export { clear, clearError, clearWarn, CUSTOM_ELEMENTS, disable, enable, error, hide, show, stringifyNodeTree, warn } from "./src/elements.js";
 export { React, ReactDOM } from "./src/lib/react_reactdom.js";
 export { clamp, map, rand, range, roundf } from "./src/math.js";
 export { Overload } from "./src/overloaded_functions.js";
--- src/console.js
+++ src/console.js
@@ -12,11 +12,11 @@ export function captureConsole() {
             console.everything.push({ type: "exception", timeStamp: TS(), value: { error, url, line } });
             return false;
         }
-        window.onunhandledrejection = function (e) { // catch some other things, idk
+        window.onunhandledrejection = function (e) { // catch some other things, IDK
             console.everything.push({ type: "promiseRejection", timeStamp: TS(), value: e.reason });
         }
         function hookLogType(logType) {
-            const original = console[logType].bind(console); // save orginal function
+            const original = console[logType].bind(console); // save original function
             console["original" + logType] = original;
             return function (...args) {
                 let info = new Error();
@@ -56,40 +56,136 @@ export function captureConsole() {
 }
 
 /**
- * @typedef ConsolePrefixOptions
- * @property {string} str - string to prefix the console log with
- * @property {string} [styles] - CSS styles to apply to the prefix
+ * @typedef {Object} LoggerOptions
+ * @property {("log" | "info" | "warn" | "error")} [type="log"] - The type of console method to use.
+ * @property {string} [=""] - The base style to apply to the log message.
+ * @property {Object} [prefix] - Options for the prefix.
+ * @property {boolean} [prefix.enabled=false] - Whether to enable the prefix.
+ * @property {string} [prefix.text="LOG"] - The text to use for the prefix.
+ * @property {string} [prefix.style="background-color:#7c7c7c;color:white;border-radius:2px;padding:2px;"] - The style to apply to the prefix.
+ * @property {Object} [brackets] - Options for the brackets.
+ * @property {boolean} [brackets.enabled=false] - Whether to enable the brackets.
+ * @property {string} [brackets.left="["] - The left bracket to use.
+ * @property {string} [brackets.right="]"] - The right bracket to use.
+ * @property {string} [brackets.style="color:#f0f;font-weight:bold;"] - The style to apply to the brackets.
+ * @property {function} [shouldLog=() => true] - A function that returns whether the log should be made. Defaults to always logging.
+ * @description
  */
 
-const defaultOptions = {
-    str: "LOG",
-    styles: "background-color:#e6e6e6;color:white;border-radius:2px"
-};
-/**
- * @param {ConsolePrefixOptions} options
- */
-export function consolePrefix(options) {
-    let { str, styles } = extend(JSON.parse(JSON.stringify(defaultOptions)), options);
-    str = `%c${str.replaceAll("%", "%%")}%c`;
-    return function (...args) {
-        if (args.length === 0) {
-            console.log(str, styles);
-        } else if (args.length === 1) {
-            if (typeof args[0] === "string") {
-                args[0] = str + " " + args[0];
-            } else {
-                args.unshift(str);
-            }
-            args.splice(1, 0, styles, "");
-            console.log(...args);
+/** @type {LoggerOptions} */
+const defaultLoggerOptions = {
+    type: "log",
+    prefix: {
+        enabled: false,
+        text: "LOG",
+        style: "background-color:#7c7c7c;color:white;border-radius:2px;padding:2px;"
+    },
+    baseStyle: "",
+    brackets: {
+        left: "[",
+        right: "]",
+        style: "color:#f0f;font-weight:bold;",
+        enabled: false
+    },
+    shouldLog: () => true
+}
+
+/** @type {<T>(obj: T) => T} */
+function copyObject(obj) {
+    /** @type {ReturnType<typeof copyObject>} */
+    const copy = {};
+    for (const key in obj) {
+        if (typeof obj[key] == "object" && obj[key] !== null) {
+            copy[key] = copyObject(obj[key]);
         } else {
-            if (typeof args[0] == "string") {
-                args[0] = str + " " + args[0];
-                args.splice(1, 0, styles, "");
-            } else {
-                args.unshift(str, styles, "");
-            }
-            console.log(...args);
+            copy[key] = obj[key];
         }
+    }
+    return copy;
+}
+
+/** @type {(source: any, target: any) => typeof source & typeof target} */
+function deepExtend(source, target) {
+    for (const key in source) {
+        if (target[key] instanceof Object && key in target) {
+            target[key] = deepExtend(source[key], target[key]);
+        } else {
+            target[key] = source[key];
+        }
+    }
+    return target;
+}
+/** @type {<A>(a: Partia<A>) => A} */
+function complete(a) {
+    return a;
+}
+const regex = /(?<embed>%[oOdisfc])|(?<raw>(?:[^%]|%[^oOdisfc]|%$)+)/g;
+export function customLogger(options = defaultLoggerOptions) {
+    options = deepExtend(options, copyObject(defaultLoggerOptions));
+    const {
+        prefix: {
+            enabled: enablePrefix,
+            text: prefixText,
+            style: prefixStyle
+        },
+        type,
+        baseStyle,
+        brackets: {
+            enabled: enableBrackets,
+            left: leftBracket,
+            right: rightBracket,
+            style: bracketsStyle
+        },
+        shouldLog
+    } = complete(options);
+    // pre-parse as much as possible to reduce overhead
+    const builder = {
+        prefix: enablePrefix ? `%c${prefixText}%c ` : "",
+        prefixStyle: enablePrefix ? [prefixStyle, baseStyle] : [],
+        leftBracket: enableBrackets ? `%c${leftBracket}%c` : "",
+        rightBracket: enableBrackets ? `%c${rightBracket}%c` : "",
+        bracketStyle: enableBrackets ? [bracketsStyle, baseStyle] : []
+    };
+    function parse(args) {
+        if (typeof args[0] !== "string") args.unshift(builder.prefix);
+        else args[0] = builder.prefix + args[0];
+
+        /** @type {string} */
+        const first_arg = args.shift();
+        const rest_args = builder.prefixStyle.concat([...args]);
+        const matches = [...first_arg.matchAll(regex)];
+        /** @type {string[]} */
+        const first_arg_result= [];
+        /** @type {any[]} */
+        const rest_arg_result = [];
+
+        matches.forEach((match) => {
+            const { embed, raw } = match.groups;
+            if (embed) {
+                const has_next_embedded = rest_args.length > 0;
+                const next_embed = rest_args.shift();
+                if (embed === "%c") {
+                    first_arg_result.push(embed);
+                    if (has_next_embedded) {
+                        rest_arg_result.push(next_embed);
+                    }
+                } else {
+                    first_arg_result.push(builder.leftBracket, embed, builder.rightBracket);
+                    rest_arg_result.push(...builder.bracketStyle.concat([next_embed]).concat(builder.bracketStyle));
+                }
+            } else if (raw) {
+                first_arg_result.push(raw);
+            }
+        });
+
+        const header = first_arg_result.join("");
+        const others = rest_arg_result.concat(rest_args);
+        if (first_arg_result.length === 0) { return others; }
+        return ["%c" + header].concat([baseStyle], others);
+    }
+    /** @type {(...args: Parameters<typeof console.log>) => void} */
+    return function (...args) {
+        if (!shouldLog()) return;
+        console[type](...parse(args));
     }
 }
